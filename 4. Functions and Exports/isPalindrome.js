177410721d061e75af3a8b371b4c2ae9b3c3c5e8
LJSantos <leanjosephsantos@gmail.com>
2024-01-16 16:44:19 +0800

    added code

--- 4. Functions and Exports/isPalindrome.js
+++ 4. Functions and Exports/isPalindrome.js
@@ -1,5 +1,11 @@
-// Todo 4.6a Create a funciton called isPalindrome function which checks if the given word is a palindrome
+// Todo 4.6a Create a function called isPalindrome function which checks if the given word is a palindrome
 // What is a Palindrome? See here: https://en.wikipedia.org/wiki/Palindrome. Feel free to search for the implementation
-
-// Todo 4.6b Set the isPalindrome function as the default export of the module
-// Useful Tutorial: https://youtu.be/qgRUr-YUk1Q?si=ceAaeHJfCYCmeNWx
+function isPalindrome(word) {
+    const cleanedWord = word.toLowerCase().replace(/[\W_]/g, ''); // Remove non-alphanumeric characters and convert to lowercase
+    const reversedWord = cleanedWord.split('').reverse().join('');
+    return cleanedWord === reversedWord;
+  }
+  
+  // Todo 4.6b Set the isPalindrome function as the default export of the module
+  // Useful Tutorial: https://youtu.be/qgRUr-YUk1Q?si=ceAaeHJfCYCmeNWx
+  module.exports = isPalindrome;
